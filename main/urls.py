f3d029ae4490e1ac925851ed510dafb57ba94e62
tturdumamatovv <muki2022@mail.ru>
2022-08-23 15:12:37 +0600

    product and rating

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from rest_framework.routers import SimpleRouter
+
+
+from product.views import ProductViewSet
+from category.views import CategoryViewSet
+
+
+
+router = SimpleRouter()
+router.register('products', ProductViewSet)
+router.register('category', CategoryViewSet)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/account/', include('account.urls')), 
+    path('api/v1/account/', include('account.urls')),
+    path('api/v1/', include(router.urls)),
 ]
--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+from product.models import Product
+
+
+User = get_user_model()
+
+
+class Mark:
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    marks = (
+        (one, 'Very Bad!'),
+        (two, 'Bad!'),
+        (three, 'Normal!'),
+        (four, 'Good!'),
+        (five, 'Excellent')
+    )
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    rating = models.ImageField(choices=Mark.marks)
+    created_at = models.DateTimeField(auto_now_add=True)
